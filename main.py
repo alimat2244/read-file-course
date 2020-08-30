d932ea5e78b760f0b7faa71858d18107792a58c2
alimat2244 <70458649+alimat2244@users.noreply.github.com>
2020-08-29 23:00:19 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+# This is a sample Python script.
+# written by Sadiat Adegbite
+
+# Enter a file name you want opened
+print("This program is written by Sadiat Adegbite")
+textfile = open(r"C:\Users\alima\OneDrive\Documents\Fruits.txt",'r')
+for i in textfile:
+    print(i, end="")
+
